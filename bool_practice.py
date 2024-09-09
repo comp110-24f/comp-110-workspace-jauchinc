d992358c67f368d7301c787ea82d30f41776da82
Jackson <jauchinc@unc.edu>
2024-09-09 15:16:10 -0400

    Practiced boolean operators and conditionals

---
+++ bool_practice.py
@@ -0,0 +1,6 @@
+def check_first_letter(word: str, letter: str) -> None:
+    """See if a word starts with a certain letter"""
+    if letter == word[0]:
+        print("Bloody fantastic!")
+    else:
+        print("What're you on about?")
